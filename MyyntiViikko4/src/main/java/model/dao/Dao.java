c676262aebb68c328d183838663015837bcca910
Olli Uronen <olli.uronen@gmail.com>
2022-04-01 23:18:55 +0300

    eka

---
+++ MyyntiViikko4/src/main/java/model/dao/Dao.java
@@ -0,0 +1,92 @@
+package model.dao;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import model.Asiakas;
+
+public class Dao {
+	private Connection con=null;
+	private ResultSet rs = null;
+	private PreparedStatement stmtPrep=null; 
+	private String sql;
+	private String db ="Myynti.sqlite";
+	
+	private Connection yhdista(){
+    	Connection con = null;    	
+    	String path = System.getProperty("catalina.base");    	
+    	path = path.substring(0, path.indexOf(".metadata")).replace("\\", "/"); //Eclipsessa
+    	//path += "/webapps/"; //Tuotannossa. Laita tietokanta webapps-kansioon
+    	String url = "jdbc:sqlite:"+path+db;    	
+    	try {	       
+    		Class.forName("org.sqlite.JDBC");
+	        con = DriverManager.getConnection(url);	
+	        System.out.println("Yhteys avattu.");
+	     }catch (Exception e){	
+	    	 System.out.println("Yhteyden avaus epäonnistui.");
+	        e.printStackTrace();	         
+	     }
+	     return con;
+	}
+	
+	public ArrayList<Asiakas> listaaKaikki(){
+		ArrayList<Asiakas> Asiakkaat = new ArrayList<Asiakas>();
+		sql = "SELECT * FROM Asiakkaat";       
+		try {
+			con=yhdista();
+			if(con!=null){ //jos yhteys onnistui
+				stmtPrep = con.prepareStatement(sql);        		
+        		rs = stmtPrep.executeQuery();   
+				if(rs!=null){ //jos kysely onnistui
+					//con.close();					
+					while(rs.next()){
+						Asiakas Asiakas = new Asiakas();	
+						Asiakas.setAsiakas_id(rs.getInt(1));
+						Asiakas.setEtunimi(rs.getString(2));
+						Asiakas.setSukunimi(rs.getString(3));
+						Asiakas.setPuhelin(rs.getString(4));	
+						Asiakas.setSposti(rs.getString(5));	
+						Asiakkaat.add(Asiakas);
+					}					
+				}				
+			}	
+			con.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}		
+		return Asiakkaat;
+	}
+	
+	public ArrayList<Asiakas> listaaKaikki(String hakusana){
+		ArrayList<Asiakas> Asiakkaat = new ArrayList<Asiakas>();
+		sql = "SELECT * FROM Asiakkaat WHERE etunimi LIKE ? or sukunimi LIKE ? or puhelin LIKE ? or sposti LIKE ?";      
+		try {
+			con=yhdista();
+			if(con!=null){ //jos yhteys onnistui
+				stmtPrep = con.prepareStatement(sql);
+				stmtPrep.setString(1, "%" + hakusana + "%");
+				stmtPrep.setString(2, "%" + hakusana + "%");   
+				stmtPrep.setString(3, "%" + hakusana + "%");    
+				stmtPrep.setString(4, "%" + hakusana + "%");  
+        		rs = stmtPrep.executeQuery();   
+				if(rs!=null){ //jos kysely onnistui
+					//con.close();					
+					while(rs.next()){
+						Asiakas Asiakas = new Asiakas();	
+						Asiakas.setAsiakas_id(rs.getInt(1));
+						Asiakas.setEtunimi(rs.getString(2));
+						Asiakas.setSukunimi(rs.getString(3));
+						Asiakas.setPuhelin(rs.getString(4));	
+						Asiakas.setSposti(rs.getString(5));		
+						Asiakkaat.add(Asiakas);
+					}					
+				}				
+			}	
+			con.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}		
+		return Asiakkaat;
+	}
+}
